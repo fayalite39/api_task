6d59b486c2e44d4310755207174cd6b3a328fe8a
fayalite39 <108684135+fayalite39@users.noreply.github.com>
2023-10-08 16:54:23 +0100

    credentials in environment; get coordinates

--- get_coordinates.py
+++ get_coordinates.py
@@ -1,18 +1,32 @@
 import argparse
+from os import getenv
 import requests
 
 
+def fetch_credentials():
+    api_key = getenv("API_KEY")
+    return api_key
+
+
 def make_api_call(ip_address):
-    api_key = "SOME_NUM"
+    api_key = fetch_credentials()
+    if not api_key:
+        return exit(1)
     ipstack_request = "http://api.ipstack.com/" + ip_address \
         + "?access_key=" + api_key
     response = requests.get(ipstack_request)
-    print(response.json())
     return response.json()
+
+
+def get_lat_long(ip_address):
+    response = make_api_call(ip_address)
+    latitude = response.get("latitude")
+    longitude = response.get("longitude")
+    print(f"{latitude}, {longitude}")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("ip_address")
     args = parser.parse_args()
-    make_api_call(args.ip_address)
+    get_lat_long(args.ip_address)
